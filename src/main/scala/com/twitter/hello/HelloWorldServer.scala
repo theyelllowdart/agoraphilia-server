98ce453d931d4dc61259476f7f5c532dcb50c339
aaron <aaron@foursquare.com>
2016-03-05 14:51:35 -0500

    yo

--- src/main/scala/com/twitter/hello/HelloWorldServer.scala
+++ src/main/scala/com/twitter/hello/HelloWorldServer.scala
@@ -7,7 +7,7 @@ import com.twitter.finatra.http.routing.HttpRouter
 
 object HelloWorldServer extends HttpServer {
 
-  override val disableAdminHttpServer = true
+  override val disableAdminHttpServer = false
 
   override def configureHttp(router: HttpRouter) {
     router
