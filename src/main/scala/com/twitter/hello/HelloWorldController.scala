7c04f84ed206a1e8681ebc344c88dc17c6bf7987
aaron <aaron@foursquare.com>
2016-03-05 18:25:35 -0500

    yoyo

--- src/main/scala/com/twitter/hello/HelloWorldController.scala
+++ src/main/scala/com/twitter/hello/HelloWorldController.scala
@@ -1,12 +1,29 @@
 package com.twitter.hello
 
-import com.twitter.finagle.http.Request
+import javax.inject.{Inject, Singleton}
+
+import com.twitter.finagle.http.{MediaType, Request}
 import com.twitter.finatra.http.Controller
+import com.twitter.finagle.http
+import com.twitter.finatra.httpclient.{RichHttpClient, HttpClient}
+import com.twitter.finatra.httpclient.modules.HttpClientModule
+import com.twitter.finatra.utils.FuturePools
+import org.joda.time.DateTime
 
-class HelloWorldController extends Controller {
+class HelloWorldController @Inject()(httpClient: HttpClient) extends Controller {
 
-  get("/hi") { request: Request =>
+  val date = DateTime.now
+
+  FuturePools.unboundedPool("yo")
+
+  get("/hi")((request: Request) => {
     info("hi")
-    "Hello " + request.params.getOrElse("name", "unnamed")
+    "Hello " + request.params.getOrElse("name", "unnamed") + date
+  })
+
+  get("/admi:*") { request: Request =>
+    //    val r = Request.apply(request.)
+    //    r.accept_=("*/*")
+    httpClient.execute(request)
   }
 }
--- src/main/scala/com/twitter/hello/HelloWorldServer.scala
+++ src/main/scala/com/twitter/hello/HelloWorldServer.scala
@@ -1,13 +1,31 @@
 package com.twitter.hello
 
+import java.net.InetSocketAddress
+
+import com.google.inject.Module
+import com.twitter.app.Flag
+import com.twitter.finagle.ListeningServer
 import com.twitter.finagle.http.{Request, Response}
 import com.twitter.finatra.http.HttpServer
 import com.twitter.finatra.http.filters.{CommonFilters, LoggingMDCFilter, TraceIdMDCFilter}
 import com.twitter.finatra.http.routing.HttpRouter
+import com.twitter.finatra.httpclient.modules.HttpClientModule
 
-object HelloWorldServer extends HttpServer {
+
+
+class AdminClient(adminServer: => ListeningServer) extends HttpClientModule {
+  lazy val port = adminServer.boundAddress.asInstanceOf[InetSocketAddress].getPort
+
+  override def dest: String = ":" + port
+}
+
+object HelloWorldMain extends HelloWorldServer
+
+class HelloWorldServer extends HttpServer {
 
   override val disableAdminHttpServer = false
+
+  override protected def modules: Seq[Module] = Vector(new AdminClient(adminHttpServer))
 
   override def configureHttp(router: HttpRouter) {
     router
